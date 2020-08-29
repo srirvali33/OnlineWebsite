814964be3afd666619a207b0a0941b387c9c82d7
srirvali33 <ravali9sri@gmail.com>
2020-08-28 22:16:44 -0500

    add commited changes

--- js/createuser.js
+++ js/createuser.js
@@ -1,18 +1,36 @@
-function createuser(){
-var frname=document.register.fname.value; 
-var Lname=document.register.lname.value;  
-var password=document.register.pswd.value; 
-var emailid=document.register.emailr.value;
+// function createuser(){
+// var frname=document.register.fname.value; 
+// var Lname=document.register.lname.value;  
+// var password=document.register.pswd.value; 
+// var emailid=document.register.emailr.value;
 
-if((Lname.length > 3) && (frname.length > 3) && (password.length > 3) && (emailid.length > 3)){
+// if((Lname.length > 3) && (frname.length > 3) && (password.length > 3) && (emailid.length > 3)){
 
-    alert("New user created Sucessfully");
+//     alert("New user created Sucessfully");
 
 
-}else{
-alert("Please enter all fields correctly");
+// }else{
+// alert("Please enter all fields correctly");
 
+
+// }
+
+// }
+
+function registerverification(){
+
+    alert("Entering page");
 
 }
 
+function over(a){
+    a.style.background= '#fcd4e2';
+}
+function out(a){
+    a.style.background= 'white';
+}
+
+function invalidemail(){
+    document.getElementById("emailerror").innerHTML="Please enter a valid email address";
+    
 }
--- js/login.js
+++ js/login.js
@@ -1,20 +1,38 @@
-function loginpage(){
+// function loginpage(){
     
-    var password=document.login.pswdl.value; 
-    var emailid=document.login.emaill.value;
-    // var ptag=document.login.getElementById("demo");
-    // print(ptag);
+//     var password=document.login.pswdl.value; 
+//     var emailid=document.login.emaill.value;
+//     // var ptag=document.login.getElementById("demo");
+//     // print(ptag);
     
-    if((password.length > 3) && (emailid.length > 3)){
+//     if((password.length > 3) && (emailid.length > 3)){
 
-        alert("login done");
+//         alert("login done");
     
-        // ptag.innerHTML=Date();
+//         // ptag.innerHTML=Date();
     
-    }else{
-    alert("Please enter all fields correctly");
+//     }else{
+//     alert("Please enter all fields correctly");
     
     
+//     }
+    
+//     }
+
+    function logverification(){
+        alert("Entering page");
     }
+
     
+
+    function over(a){
+        a.style.background= '#fcd4e2';
+    }
+    function out(a){
+        a.style.background= 'white';
+    }
+
+    function invalidemail(){
+        document.getElementById("emailerror").innerHTML="Please enter a valid email address";
+        
     }
